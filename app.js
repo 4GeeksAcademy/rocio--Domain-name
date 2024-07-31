f948c9b229f59d2aca33dec6d104e629f2d55370
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-07-31 20:53:13 +0000

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
-let format=['.com']
+let format='.com';
 
 const email=document.getElementById("email");
 
 for (let i=0; i<pronoun.length;i++){
     for(let j=0; j<adj.length;j++){
         for(let k=0; k<noun.length;k++){
-            for(let l=0; l<noun.length;l++){
-                let result=pronoun[i]+adj[j]+noun[k]+format[l];
+                let result=pronoun[i]+adj[j]+noun[k]+format;
                 console.log(result);
                 email.innerHTML+=`<li>${result}</li>`
-            }
-
         }
     }
 }
